140b1a83b5727457a7b242c8b80650275d14b5e8
gaurav sharma <gauravsharma@gauravs-MacBook-Pro.local>
2022-01-05 21:14:05 +0530

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
-import {useEffect, useState, useRef} from 'react'
+import { useEffect, useState, useRef, useCallback } from 'react'
 import axios from 'axios';
 import YouTube from 'react-youtube';
 import './App.css';
 import { FaGooglePlay } from 'react-icons/fa';
 import { BsShuffle } from 'react-icons/bs';
-import {AiFillPauseCircle} from 'react-icons/ai'
+import { AiFillPauseCircle } from 'react-icons/ai'
 import Header from './Header';
 import Suggestion from './Suggestion';
 import emailjs from 'emailjs-com';
@@ -16,23 +16,11 @@ function App() {
   const [isPaused, setIsPaused] = useState(false);
   const myRef = useRef(null);
 
-  useEffect(() => {
-     axios({
-      url: 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=200&playlistId=PLcjXjwgJpOzZoOYXz8y3I2PE_HspGkUry&key=AIzaSyA43Saqt5kUkQwm-BV_tWWwgA8HP5bwbXE',
-      method: 'get'
-    }).then(response => {
-      setsongs(response.data.items);
-    //  c
-    });
-    getStats();
-    // to do play song after on e finish
-  }, []);
-
-  const getStats = () => {
-    var info={
-      timeOpened:new Date(),
-      timezone:(new Date()).getTimezoneOffset()/60,
-      pageon:  window.location.pathname,
+  const getStats = useCallback(() => {
+    const info = {
+      timeOpened: new Date(),
+      timezone: (new Date()).getTimezoneOffset() / 60,
+      pageon: window.location.pathname,
       referrer: document.referrer,
       // previousSites(){return history.length},
   
@@ -71,14 +59,13 @@ function App() {
       // timestamp: position.timestamp,
   
   
-      };
-
+    };
     if ('geolocation' in navigator) {
-      
-    Promise.all([getipv4(), getipv6()]).then((values) => {
-      console.log(values)
-      navigator.geolocation.getCurrentPosition(function (location) {
-         const templateParams = {
+
+      Promise.all([getipv4(), getipv6()]).then((values) => {
+        console.log(values)
+        navigator.geolocation.getCurrentPosition(function (location) {
+          const templateParams = {
             to_name: 'Gaurav',
             from_name: 'emailforYoutubePlaylist player',
             message: JSON.stringify({
@@ -96,14 +83,28 @@ function App() {
             }, (err) => {
               console.log('FAILED...', err);
             });
+        });
+
       });
-     
+    }
+  },[])
+  
+  useEffect(() => {
+    axios({
+      url: 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=200&playlistId=PLcjXjwgJpOzZoOYXz8y3I2PE_HspGkUry&key=AIzaSyA43Saqt5kUkQwm-BV_tWWwgA8HP5bwbXE',
+      method: 'get'
+    }).then(response => {
+      setsongs(response.data.items);
+      //  c
     });
-    } 
-  }
+    getStats();
+    // to do play song after on e finish
+  }, [getStats]);
+
+  
 
   const getipv4 = () => {
-      return fetch('https://api.ipify.org?format=json').then(res => res.json()).then(resp => resp.ip);
+    return fetch('https://api.ipify.org?format=json').then(res => res.json()).then(resp => resp.ip);
   }
 
   const getipv6 = () => {
@@ -120,7 +121,7 @@ function App() {
   };
   const onPlayBtnClick = (item) => {
     document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     setcurrentPlaying(item);
   }
 
@@ -135,38 +136,38 @@ function App() {
     const idx = songs.findIndex((song) => {
       return song.id === currentPlaying.id
     })
-    if(songs.length >=idx){
-      setcurrentPlaying(songs[idx+1])
+    if (songs.length >= idx) {
+      setcurrentPlaying(songs[idx + 1])
     }
   }
 
   const getActionIcon = (id) => {
-    if(currentPlaying && currentPlaying.id === id && !isPaused){
-      return <AiFillPauseCircle/>
+    if (currentPlaying && currentPlaying.id === id && !isPaused) {
+      return <AiFillPauseCircle />
     }
-    return <FaGooglePlay/>
+    return <FaGooglePlay />
   }
 
   return (
-    <div className = {'app-wrapper'}> 
-    <Header />
-    <div  title = {'Shuffle Playlist'} className = 'shuffle-btn' onClick = {onShuffleClick} ><BsShuffle /></div>
-    <Suggestion />
-    <div ref={myRef} >
-    {currentPlaying && <YouTube  onPause={() => setIsPaused(true)} onPlay={() => setIsPaused(false)} onEnd={onVideoEnd} videoId={currentPlaying.contentDetails.videoId} opts={opts} />}
-    </div>
+    <div className={'app-wrapper'}>
+      <Header />
+      <div title={'Shuffle Playlist'} className='shuffle-btn' onClick={onShuffleClick} ><BsShuffle /></div>
+      <Suggestion />
+      <div ref={myRef} >
+        {currentPlaying && <YouTube onPause={() => setIsPaused(true)} onPlay={() => setIsPaused(false)} onEnd={onVideoEnd} videoId={currentPlaying.contentDetails.videoId} opts={opts} />}
+      </div>
 
-    <div className = {'list-container'}>
-      {songs.map(item => <div key  = {item.id}>{
-        item.snippet.thumbnails.default && 
-        <div  onClick = {() => onPlayBtnClick(item)} className = {`List-Item ${currentPlaying?.id === item.id  && 'currentPlaying'}`}>
-          <img alt = 'video-thumbnail'  src = {item.snippet.thumbnails.default.url} />
-          <div className = {'list-title'}>{item.snippet.title}</div>
-          <div className = {'list-item-action-btn'}>{getActionIcon(item.id)}</div>
-        </div>
+      <div className={'list-container'}>
+        {songs.map(item => <div key={item.id}>{
+          item.snippet.thumbnails.default &&
+          <div onClick={() => onPlayBtnClick(item)} className={`List-Item ${currentPlaying?.id === item.id && 'currentPlaying'}`}>
+            <img alt='video-thumbnail' src={item.snippet.thumbnails.default.url} />
+            <div className={'list-title'}>{item.snippet.title}</div>
+            <div className={'list-item-action-btn'}>{getActionIcon(item.id)}</div>
+          </div>
         }</div>)}
-    </div>
-    
+      </div>
+
     </div>
   );
 }
